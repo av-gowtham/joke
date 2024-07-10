de81ceea0dfb19000735dcf5b0a9600dd7341a72
av-gowtham <gowthamavg10@gmail.com>
2024-07-10 11:45:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [joke, setJoke] = useState("Click the below button to get a joke");
+  const [count, setCount] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  async function handleJoke() {
+    if (loading) {
+      return;
+    }
+    try {
+      setLoading(true);
+      const res = await fetch(
+        "https://geek-jokes.sameerkumar.website/api?format=json"
+      );
+      const data = await res.json();
+      setJoke(data.joke);
+      setCount((c) => c + 1);
+    } catch (e) {
+      console.log("Error fetching joke: ", e);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    handleJoke();
+  }, []);
+
+  return (
+    <div>
+      <p className="joke">{joke}</p>
+      <button onClick={handleJoke}>
+        {loading ? "loading..." : "Get jokes"}
+      </button>
+      <p className="count">
+        You have read <b>{count}</b> {count <= 1 ? "piece" : "pieces"} of jokes
+      </p>
+    </div>
+  );
+}
+
+export default App;
